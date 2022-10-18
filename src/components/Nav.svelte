43a8f9b25c447affe74e184c2fd72e3175c76298
David <djaime93@gmail.com>
2022-10-17 21:37:01 -0500

    mend

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -1,36 +1,37 @@
 <script>
-  import { page } from '$app/stores';
-  import SignoutButton from '../components/SignoutButton.svelte';
-  import WalletButton from './WalletButton.svelte';
+	import { page } from '$app/stores';
+	import SignoutButton from '../components/SignoutButton.svelte';
+	import WalletButton from './WalletButton.svelte';
 
-  const style = {
-    wrapper: `p-6 flex flex-cols justify-between`,
-    left: ``,
-    nav: ``,
-    user: `flex flex-cols gap-4`
-  }
+	const style = {
+		wrapper: `p-6 flex flex-cols justify-between`,
+		left: ``,
+		nav: ``,
+		user: `flex flex-cols gap-4`,
+		loginButton: `p-2 bg-black text-white`
+	};
 </script>
 
 <div class={style.wrapper}>
-  <div>
-    <a href="/">Home</a>
-  </div>
+	<div>
+		<a href="/">Home</a>
+	</div>
 
-  <div class={style.nav}>
-    {#if $page.data.session.user}
-      <a href="/dashboard">dashboard</a>
-    {/if}
-  </div>
+	<div class={style.nav}>
+		{#if $page.data.session.user}
+			<a href="/dashboard">dashboard</a>
+		{/if}
+	</div>
 
-  <div class={style.user}>
-    {#if !$page.data.session.user}
-      <a href="/login">Login</a>
-    {:else}
-      <!-- <p>I am logged in!</p> -->
-      <a href="/profile">{$page.data.session.user.email?.split("@")[0]}</a>
-      <SignoutButton/>
+	<div class={style.user}>
+		{#if !$page.data.session.user}
+			<a class={style.loginButton} href="/login">Login</a>
+		{:else}
+			<!-- <p>I am logged in!</p> -->
+			<a href="/profile">{$page.data.session.user.email?.split('@')[0]}</a>
+			<SignoutButton />
 
-      <WalletButton/>
-    {/if}
-  </div>
+			<WalletButton />
+		{/if}
+	</div>
 </div>
--- src/components/WalletButton.svelte
+++ src/components/WalletButton.svelte
@@ -4,10 +4,15 @@
 	const connectWallet = () => {
 		connected = true;
 	};
+
+	const style = {
+		connectButton: `p-2 bg-black text-white`,
+		connectedButton: `p-2 bg-gray-400 text-white`,
+	}
 </script>
 
 {#if !connected}
-	<div on:click={() => connected = !connected}>Connect Wallet</div>
+	<div class={style.connectButton} on:click={() => connected = !connected}>Connect Wallet</div>
 {:else}
-    <div on:click={() => connected = !connected}>0x34..3432</div>
+    <div class={style.connectedButton} on:click={() => connected = !connected}>0x34..3432</div>
 {/if}
